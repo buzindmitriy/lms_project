f228e7c829349d86815352847c6c39e1936b731b
Buzin Dmitriy <89111703879@mail.ru>
2025-03-27 18:57:09 +0300

    add documentary urls

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Online Studying API",
+        default_version='v1',
+        description="API для управления курсами, уроками и платежами",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +26,7 @@ urlpatterns = [
     path('api/users/', include('users.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
